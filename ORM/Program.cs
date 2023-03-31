a464b774a05a5211883828b218790c00006f6c4a
Dmitriy <123815654+Cabalette@users.noreply.github.com>
2023-03-31 12:16:24 +0300

    upd1

--- ORM/Program.cs
+++ ORM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using System.Reflection.Metadata;
 
 namespace ORM
@@ -9,22 +10,44 @@ namespace ORM
         {
             using (var context = new Test1Context())
             {
-                var query = context.Members.Select(x => new
-                {
-                    Surname = x.Surname,
-                    Firstname = x.Firstname,
-                    Joindate = x.Joindate
-                }).Where(x =>x.Joindate== context.Members.Max(x => x.Joindate));
-
+                var query = (from a in context.Members
+                            join b in context.Bookings on a.Memid equals b.Memid
+                            join c in context.Facilities on b.Facid equals c.Facid
+                            where c.Name =="Tennis Court 2" || c.Name == "Tennis Court 1"                           
+                            select new
+                            {
+                                member = a.Firstname + " " + a.Surname,
+                                facility = c.Name
+                            }).Distinct().OrderBy(x=>x.member).ThenBy(x=>x.facility);
+                //var query = context.Members.Select(x => new
+                //{
+                //    MemSurname = x.Surname,
+                //    MemFirstname = x.Firstname,
+                //    Recommendedby = x.Recommendedby
+                //}).LeftJoin(context.Members.Select(x => new
+                //{
+                //    RecSurname = x.Surname,
+                //    RecFirstname = x.Firstname,
+                //    Memid= x.Memid
+                //}), a => a.Recommendedby, b => b.Memid, (a, b) => new
+                //{
+                //    memfname = a.MemFirstname,
+                //    memsname = a.MemSurname
+                //    //recfname=b.RecFirstname,
+                //    //recsname=b.RecSurname
+                //}).OrderBy(x=>x.memsname).ThenBy(x=> x.memfname);
+                Console.WriteLine( "Member \t\t\t Facility" );
                 foreach (var stuff in query)
                 {
                     Console.WriteLine($"" +
                     // $"facid - {stuff.Memid} " +
-                    $"- Surname - {stuff.Surname}" +
-                    $" \t Firstname {stuff.Firstname}" +
-                    $"- Joindate - {stuff.Joindate}" +
-                    //$"\t - guestcost - {stuff.Guestcost} \t " +
-                    //$"- initialoutlay - {stuff.Initialoutlay} \t" +
+                    $" - {stuff.member}" +
+                    $" - {stuff.facility}" +
+                    // $"- Surname - {stuff.recfname}" +
+                    //$" \t Firstname {stuff.recsname}" +
+                    //$"- Joindate - {stuff.Joindate}" +
+                    //$"\t - Start - {stuff.Start} \t " +
+                    //$"- Name - {stuff.Name} \t" +
                     //$"- monthlymaintenance - {stuff.Monthlymaintenance}" +
                     " ");
 
@@ -97,5 +120,70 @@ var query = context.Members.Select(x => new
                     Firstname = x.Firstname,
                     Joindate = x.Joindate
                 }).Where(x =>x.Joindate== context.Members.Max(x => x.Joindate)); агрегатная функция в селекте
+
+var query = context.Bookings.Select(x => new
+                {
+                    Memid = x.Memid,
+                    Starttime = x.Starttime
+                }).Join(context.Members.Select(x => new
+                {
+                    Memid = x.Memid,
+                    Firstname = x.Firstname,
+                    Surname = x.Surname
+                }).Where(x => x.Firstname == "David" && x.Surname == "Farrell"), b => b.Memid, m => m.Memid, (b, m) => new
+                {
+                    Starttime = b.Starttime
+                });         inner join + where
+
+var query = context.Facilities.Select(x => new
+                {
+                    Facid = x.Facid,
+                    Name = x.Name
+                }).Where(x => x.Name == "Tennis Court 2" || x.Name == "Tennis Court 1").Join(context.Bookings.Select(x => new
+                {
+                    Facid = x.Facid,
+                    Start = x.Starttime
+                }).Where(x => x.Start >= DateTime.Parse("2012-09-21") && x.Start < DateTime.Parse("2012-09-22")), b => b.Facid, m => m.Facid, (b, m) => new
+                {
+                    Start = m.Start,
+                    Name=b.Name
+                });      еще один inner join + where
+
+var query = context.Members.Select(x => new
+                {
+                    Recommendedby = x.Recommendedby
+                }).Join(context.Members.Select(x => new
+                {
+                    Surname = x.Surname,
+                    Firstname = x.Firstname,
+                    Memid= x.Memid
+                }), a => a.Recommendedby, b => b.Memid, (a, b) => new
+                {
+                    Firstname = b.Firstname,
+                    Surname = b.Surname
+                }).Distinct().OrderBy(x=>x.Surname).ThenBy(x=> x.Firstname);  self join + order по 2 полям
+
+var query = from a in context.Members
+                            join b in context.Members on a.Recommendedby equals b.Memid into newtable
+                            from subshit in newtable.DefaultIfEmpty()
+                            orderby a.Surname,a.Firstname
+                            select new
+                            {
+                                memfname = a.Firstname,
+                                memsname = a.Surname,
+                                recfname = subshit == null ? string.Empty : subshit.Firstname,
+                                recsname = subshit == null ? string.Empty : subshit.Surname
+                            };  left join
+
+var query = (from a in context.Members
+                            join b in context.Bookings on a.Memid equals b.Memid
+                            join c in context.Facilities on b.Facid equals c.Facid
+                            where c.Name =="Tennis Court 2" || c.Name == "Tennis Court 1"                           
+                            select new
+                            {
+                                member = a.Firstname + " " + a.Surname,
+                                facility = c.Name
+                            }).Distinct().OrderBy(x=>x.member).ThenBy(x=>x.facility);  тройной JOIN + distinct + 2 order
+
 
  */
